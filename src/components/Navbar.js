ff5d12f917123f3116838b617dea08be09adf724
Allen <allen0321.lin@ehsn.com.tw>
2021-05-18 15:02:24 +0800

    Navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,45 @@
 ﻿import React from "react";
 import { Link } from "gatsby";
-
+import { FiAlignJustify } from "react-icons/fi";
+import logo from "../assets/images/logo.svg";
 const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/tags">Tags</Link>
-                </li>
-                <li>
-                    <Link to="/recipes">Recipes</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <buttom className="nav-btn">
+            <FiAlignJustify />
+          </buttom>
+        </div>
+        <div className="nav-links show-links">
+          <Link className="nav-link" activeClassName="active-link" to="/">
+            Home
+          </Link>
+          <Link className="nav-link" activeClassName="active-link" to="/about">
+            About
+          </Link>
+          <Link className="nav-link" activeClassName="active-link" to="/tags">
+            Tags
+          </Link>
+          <Link
+            className="nav-link"
+            activeClassName="active-link"
+            to="/recipes"
+          >
+            Recipes
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
